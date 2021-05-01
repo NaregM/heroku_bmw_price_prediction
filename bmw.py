45e2ed0d4d835edd951b20c9c7605e430f894072
NaregM <mirzatun@usc.edu>
2021-04-30 19:39:16 -0700

    remove np

--- bmw.py
+++ bmw.py
@@ -1,10 +1,10 @@
 #import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 import os
 
-import seaborn as sns
+#import seaborn as sns
 
 import streamlit as st
 
